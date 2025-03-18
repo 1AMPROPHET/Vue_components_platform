4916085bbafe9caed9b79435b2ee6276b9c7a276
wangyuanfeng（王元锋） <10033863@inovance.local>
2025-03-18 19:45:17 +0800

    first commit, a paltform for vue3 components

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 8888,
+    open: false,
+    cors: true,
+  },
+});
